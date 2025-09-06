7c3126becfbd97e28407507e7c65820c44a1459a
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2025-09-06 16:09:28 -0300

    feat: style adjustments

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,16 +1,18 @@
 import { Image, View } from "react-native";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 import { styles } from "./style";
-import { Input } from "@/components/Input";
 
 export const Home = () => {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você deseja comprar?" />
-      <Button text="Adicionar" activeOpacity={0.5} />
+      <View style={styles.form}>
+        <Input placeholder="O que você deseja comprar?" />
+        <Button text="Adicionar" activeOpacity={0.5} />
+      </View>
     </View>
   );
 };
