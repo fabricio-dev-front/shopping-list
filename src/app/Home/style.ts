498a11e72755c16989695397d9e6b570aadce626
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2025-09-11 08:54:15 -0300

    feat: add style

--- src/app/Home/style.ts
+++ src/app/Home/style.ts
@@ -23,10 +23,26 @@ export const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     marginTop: 30,
-    paddingTop: 24,
+    paddingTop: 30,
     paddingHorizontal: 24,
     borderTopLeftRadius: 24,
     borderTopRightRadius: 24,
     backgroundColor: "#1A1A1A",
+  },
+  headerFilters: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#333",
+    paddingBottom: 12,
+  },
+  buttonClear: {
+    marginLeft: "auto",
+  },
+  clearText: {
+    fontSize: 14,
+    color: "#828282",
+    fontWeight: "600",
   },
 });
