9d10c5bb7a4be8db82c198f43f86280b478cc5e4
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2025-09-15 08:51:31 -0300

    feat: using FlatList

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View, TouchableOpacity, Text } from "react-native";
+import { Image, View, TouchableOpacity, Text, FlatList } from "react-native";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -9,6 +9,7 @@ import { styles } from "./style";
 import { FilterStatus } from "@/types/FilterStatus";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING];
+const ITEMS_MOCK = Array.from({ length: 100 }).map((_, index) => String(index));
 
 export const Home = () => {
   return (
@@ -30,11 +31,19 @@ export const Home = () => {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-
-        <Item
-          data={{ status: FilterStatus.DONE, description: "Comprar pão" }}
-          onStatus={() => {}}
-          onRemove={() => {}}
+        <FlatList
+          data={ITEMS_MOCK}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Item
+              data={{
+                status: FilterStatus.DONE,
+                description: `Comprar pão ${item}`,
+              }}
+              onStatus={() => {}}
+              onRemove={() => {}}
+            />
+          )}
         />
       </View>
     </View>
