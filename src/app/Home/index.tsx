5e348294bc74723a5431b22fdaf32398cfac5fcc
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2025-09-16 07:46:43 -0300

    feat: using useState to control the filter

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Image, View, TouchableOpacity, Text, FlatList } from "react-native";
 
 import { Button } from "@/components/Button";
@@ -9,9 +10,19 @@ import { styles } from "./style";
 import { FilterStatus } from "@/types/FilterStatus";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING];
-const ITEMS_MOCK = Array.from({ length: 100 }).map((_, index) => String(index));
+
+const ITEMS_MOCK = [
+  { id: "1", status: FilterStatus.DONE, description: "Comprar pão" },
+  { id: "2", status: FilterStatus.PENDING, description: "Comprar leite" },
+  { id: "3", status: FilterStatus.DONE, description: "Comprar ovos" },
+  { id: "4", status: FilterStatus.PENDING, description: "Comprar frutas" },
+  { id: "5", status: FilterStatus.DONE, description: "Comprar verduras" },
+  { id: "6", status: FilterStatus.PENDING, description: "Comprar carne" },
+];
 
 export const Home = () => {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
@@ -24,25 +35,30 @@ export const Home = () => {
       <View style={styles.content}>
         <View style={styles.headerFilters}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.buttonClear}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
+
         <FlatList
           data={ITEMS_MOCK}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <Item
-              data={{
-                status: FilterStatus.DONE,
-                description: `Comprar pão ${item}`,
-              }}
-              onStatus={() => {}}
-              onRemove={() => {}}
-            />
+            <Item data={item} onStatus={() => {}} onRemove={() => {}} />
+          )}
+          showsHorizontalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyText}>Nenhum item adicionado.</Text>
           )}
         />
       </View>
